51084c4ff8bc0371ad06a9d0fc052cf047de832b
SisFranco <sisfranco8@gmail.com>
2025-11-18 13:02:24 -0500

    Modulo de productos

--- repo_md/src/product/product.controller.ts
+++ repo_md/src/product/product.controller.ts
@@ -1,4 +1,118 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateProductDto } from './dtos/createProduct.dto';
+import { UpdateProductDto } from './dtos/updateProduct.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ProductsService } from './product.service';
 
-@Controller('product')
-export class ProductController {}
+@ApiTags('Productos')
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  //Ruta para obtener un producto por UUID:
+  @ApiOperation({ summary: 'Obtener un producto por UUID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Producto obtenido exitosamente.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'No existe un producto con el UUID ingresado.',
+  })
+  @ApiResponse({ status: 500, description: 'Error interno del servidor.' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(UserRoles.ADMIN, UserRoles.PRODUCER)
+  @Get('getProduct/:uuid')
+  getProductById(@Param('uuid', ParseUUIDPipe) uuid: string) {
+    return this.productsService.getProductById(uuid);
+  }
+
+  //Ruta para obtener productos por su categoría:
+  @ApiOperation({ summary: 'Obtener productos filtrados por categoría' })
+  @ApiResponse({
+    status: 200,
+    description: 'Productos obtenidos exitosamente.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'No existe una categoría con el UUID ingresado.',
+  })
+  @ApiResponse({ status: 500, description: 'Error interno del servidor.' })
+  @Get('category/:categoryUuid')
+  getProductsByCategory(
+    @Param('categoryUuid', ParseUUIDPipe) categoryUuid: string,
+  ) {
+    return this.productsService.getProductsByCategory(categoryUuid);
+  }
+
+  //Ruta para crear un producto:
+  @ApiOperation({ summary: 'Crear un nuevo producto' })
+  @ApiResponse({ status: 201, description: 'Producto creado exitosamente.' })
+  @ApiResponse({ status: 400, description: 'Datos inválidos en el DTO.' })
+  @ApiResponse({
+    status: 409,
+    description: 'Ya existe un producto con el código o nombre ingresado.',
+  })
+  @ApiResponse({ status: 500, description: 'Error interno del servidor.' })
+  @ApiBearerAuth()
+  @Roles(UserRoles.ADMIN, UserRoles.PRODUCER)
+  @Post('createProduct')
+  postCreateProduct(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.postCreateProduct(createProductDto);
+  }
+
+  //Ruta para actualizar un producto:
+  @ApiOperation({ summary: 'Actualizar un producto' })
+  @ApiResponse({
+    status: 200,
+    description: 'Producto actualizado exitosamente.',
+  })
+  @ApiResponse({ status: 400, description: 'Datos inválidos en el DTO.' })
+  @ApiResponse({
+    status: 404,
+    description: 'No existe un producto con el UUID ingresado.',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Ya existe un producto con el código o nombre ingresado.',
+  })
+  @ApiResponse({ status: 500, description: 'Error interno del servidor.' })
+  @ApiBearerAuth()
+  @Roles(UserRoles.ADMIN, UserRoles.PRODUCER)
+  @Put('updateProduct')
+  putUpdateProduct(@Body() updateProductDto: UpdateProductDto) {
+    return this.productsService.putUpdateProduct(updateProductDto);
+  }
+
+  //Ruta para desactivar un producto:
+  @ApiOperation({ summary: 'Desactivar un producto' })
+  @ApiResponse({
+    status: 200,
+    description: 'Producto desactivado correctamente.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'UUID inválido.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'No existe un producto activo con el UUID ingresado.',
+  })
+  @ApiResponse({ status: 500, description: 'Error interno del servidor.' })
+  @ApiBearerAuth()
+  @Roles(UserRoles.ADMIN, UserRoles.PRODUCER)
+  @Delete('deleteProduct/:uuid')
+  deleteProduct(@Param('uuid', ParseUUIDPipe) uuid: string) {
+    return this.productsService.deleteProduct(uuid);
+  }
+}
--- repo_md/src/product/product.module.ts
+++ repo_md/src/product/product.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from 'src/entities/products.entity';
+import { BusinessModule } from 'src/business/business.module';
+import { CategoriesModule } from 'src/category/category.module';
+import { ProductsController } from './product.controller';
+import { ProductsService } from './product.service';
+import { ProductsRepository } from './repository';
 
-@Module({})
-export class ProductModule {}
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+    CategoriesModule,
+    BusinessModule,
+  ],
+  controllers: [ProductsController],
+  providers: [ProductsService, ProductsRepository],
+})
+export class ProductsModule {}
